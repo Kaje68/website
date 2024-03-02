ea5ee3543df96957503c4d0bb036ccdbdb35f24a
Kaje68 <5506715+Kaje68@users.noreply.github.com>
2024-03-02 18:27:12 -0500

    Scrubby update 3
    Slowly dialing in on basic creation of deck and adding one flashcard at a time through "deck management"(will eventually change that to allow for multiple additions, moving, edits, etc.
    
    Currently working on "studying" with the in tent of selecting a deck and then having one face showing with a flip and next option.  Getting weird behavior with a deck title of "D10" always routing incorrectly.
    
    Also just watched another tutorial while I was on the treadmill.  T his guy was writing a basic Todo list using a databvase similar to techwithtims tutorial that wrote a notes page and where I started.  Techwithtim used .js with button onclicks to interface with the database... while the todo list tutorial never used .js and did it directly to Python with hrefs.  Guy on Reddit pointed out I shouldn't be doing onclicks as well so got to figure that out.

--- static/index.js
+++ static/index.js
@@ -3,7 +3,7 @@ function deleteDeck(deckId) {
     method: "POST",
     body: JSON.stringify({ deckId: deckId }),
   }).then((_res) => {
-    window.location.href = "/";
+    window.location.href = "/managedecks";
   });
 }
 
@@ -15,7 +15,7 @@ function deleteFlash(flashId) {
     window.location.href = "/";
   });
 }
-
+// I believe .js is only needed for POSTS when wanting to write to the db
 function fetchFlash(flashId){
   fetch("/fetch-flash", {
     method: "POST",
@@ -27,6 +27,7 @@ function fetchFlash(flashId){
 }
 
 // USed to toggle show/hide the list of flashcards belonging to referenced deck. 
+// with new menu setup this is not needed
 function fetchDeck(deckTitle){
   
   var flash_list = document.getElementById(deckTitle);
@@ -38,6 +39,24 @@ function fetchDeck(deckTitle){
  
 
 }
+// I believe .js is only needed for POSTS when wanting to write to the db
+function fetchDeck2(deckTitle){
+  console.log(deckTitle);
+  fetch("/managesingledeck/<deckTitle>", {
+    method: "POST",
+    body: JSON.stringify({deckTitle: deckTitle}),
+  }).then((_res) => {
+    // window.location.href = "/managesingledeck/<deckTitle>";
+  });
+}
+
+
+
+
+
+
+
+
 
 
 //this "flips" the flash card by hiding one side and showing the other
@@ -63,5 +82,48 @@ function flipFlash() {
 
 
 
+// stole this from https://phuoc.ng/collection/html-dom/make-a-text-area-auto-expand/
+  // const textareaEle = document.getElementById('f_title_textarea');
+  // textareaEle.addEventListener('input', () => {
+  //     // textareaEle.style.height = 'auto';      //  not sure why this is here. 
+  //     textareaEle.style.height = `${textareaEle.scrollHeight}px`;
+  // });
+// will autosize a textarea  with specifc id automatically
+// had to make a lot of modifications to fit multiple text boxes in a grid.  
+document.addEventListener('DOMContentLoaded', () => {
+
+  const grid_mta = document.querySelectorAll(".grid_main_textarea");
+  grid_mta.forEach(g_mta => {
+    g_mta.addEventListener("input", () => {
+      g_mta.style.height = '6.1em';
+      g_mta.style.height = `${g_mta.scrollHeight}px`;
+    })
+
+  })
+
+  const header_ta = document.querySelectorAll(".header");
+  header_ta.forEach(h_ta => {
+    h_ta.addEventListener("input", () => {
+      // h_ta.style.height = '2';
+      h_ta.style.height = `${h_ta.scrollHeight}px`;
+
+    })
+
+  })
+});
 
 
+// how to do a popup
+function deck_title_popup() {
+
+
+  let text;
+  let deckTitle = prompt("Please enter Kwik Deck Title:", "");
+  if (deckTitle == null || deckTitle == "") {
+    window.location.href = "/managedecks";
+  } 
+  document.getElementById("new_deck_title").innerHTML = deckTitle;
+  // window.location.href = "/managesingledeck/<deckTitle>";
+
+
+}
--- views.py
+++ views.py
@@ -10,23 +10,220 @@ from . import db
 import json
 from datetime import datetime
 
-
-
-
 #this file is a blueprint of our application, it has a bunch of *routes* ie url's
 views = Blueprint("views", __name__)     #doesn't have to be name of file but seems to be easier. 
 
 fetched_flash = Flashcard()
-@views.route('/', methods = ["GET", "POST"])       #this is homepage route(url)
+
+@views.route('/', methods = ["GET"])       #this is homepage route(url)
 @login_required
 def home():
-    print('hit')
+        return render_template("home.html", user=current_user)
+
+
+@views.route('/managedecks', methods=['GET', 'POST'])
+def managedecks():
+    return render_template("managedecks.html", user = current_user)
+
+
+@views.route('/study_pick_deck', methods=['GET', 'POST'])
+def study_pick_deck():
+    return render_template("study_pick_deck.html", user = current_user)
+
+
+@views.route('/study_deck/<deckTitle>', methods=['GET'])
+def study_deck(deckTitle):
+    deck = Deck.query.get(deckTitle)
+    # print(deck)
+    # print(len(deck.flashcards))
+    print(request.endpoint)
+    return render_template("study_deck.html", user = current_user, deck = deck)
+
+
+
+@views.route('/managesingledeck/<deckTitle>', methods=['POST', 'GET'])
+def managesingledeck(deckTitle):
+    print('hit here decktitle')
+    # print(request.method)
+    print(request.data)
+    # deck = json.loads(request.data)
+    # deckTitle = deck["deckTitle"]
+    deck = Deck.query.get(deckTitle)
+    print(deck)
+    if request.method == "POST":
+        print(request.form)
+        f_title = request.form.get("f_title")
+        f_front = request.form.get("f_front")
+        f_back = request.form.get("f_back")
+        if f_title is None:
+            print("wtf")
+            return redirect("home")
+
+        if len(f_title) < 1:
+            flash("Flashcard must have a title!", category = "error")
+        if len(f_front) < 1:
+            flash("Front must be populated!", category = "error")
+        if len(f_back) < 1:
+            flash("Back must be populated!", category = "error")
+        else:
+            # new_deck = Deck(title = deck_title, user_id = current_user.id )
+            new_flash = Flashcard(title = f_title, data = f_front, data2 = f_back, deck_title = deckTitle, user_id = current_user.id) 
+            
+            db.session.add(new_flash)
+            db.session.commit()
+
+            # return redirect("/")                #This is to facilitate(I hope) post-redirect-get design to stop accidental duplicates upon browser refresh
+   
+
+    return render_template("managesingledeck.html", user = current_user, deck = deck)
+
+
+@views.route('/createDeck/', methods=['POST', 'GET'])
+def createDeck():
+    
+    if request.method == "GET":
+        return render_template("createDeck.html", user = current_user)
+    
+
+    deck_title = request.form.get("deck_title")
+    flash_title = request.form.get("f_title")
+    flasha = request.form.get("f_front")
+    flashb = request.form.get("f_back")
+    bad_entry = False
+    deck = Deck.query.get(deck_title)
+    if deck: 
+        flash("Deck already exists!", category = "error")
+        return redirect("/createDeck")
+        # render_template("managesingledeck.html", user = current_user, deck = deck)
+        # url = "/managesingledeck/" + deck_title
+        # return redirect(url)
+
+    if len(deck_title) < 1:
+        flash("Deck must have a title!", category = "error")
+        bad_entry = True
+    if len(flash_title) < 1:
+        flash("Flashcard must have a title!", category = "error")
+        bad_entry = True
+    if len(flasha) < 1:
+        flash("Side A must be populated!", category = "error")
+        bad_entry = True
+    if len(flashb) < 1:
+        flash("Side B must be populated!", category = "error")
+        bad_entry = True
+    if not bad_entry:
+        print("hit")
+        new_deck = Deck(title = deck_title, user_id = current_user.id )
+        new_flash = Flashcard(title = flash_title, data = flasha, data2 = flashb, deck_title = new_deck.title, user_id = current_user.id) 
+        db.session.add(new_deck)
+        db.session.add(new_flash)
+        db.session.commit()
+        flash("Kwik Deck created successfully!", category = "success")
+        
+    return redirect("/createDeck")
+
+
+
+
+
+
+
+
+
+
+#
+# deletes deck and all flashcards.  Will want to change this as to allow a flash card toe xist in multiple decks. 
+#
+@views.route("delete-deck", methods = ["POST", "GET"])
+def delete_deck():
+    print('delete_deck method ' + request.method)
+    deck = json.loads(request.data)
+    deckId = deck["deckId"]
+
+    deck = Deck.query.get(deckId)
+    if deck:
+        if deck.user_id == current_user.id:
+            for flashcard in deck.flashcards:
+                db.session.delete(flashcard)
+            db.session.delete(deck)
+            db.session.commit()
+    
+    return jsonify({})
+
+#
+# deletes a single flash card. 
+#
+@views.route("/delete-flash", methods = ["POST", "GET"])
+def delete_note():
+    print('delete_note method ' + request.method)
+    flashcard = json.loads(request.data)
+    flashId = flashcard["flashId"]
+    flashcard = Flashcard.query.get(flashId)
+    #print(note.user.email)                     demonstrates how backref works(see models.py and db.relationship(...))
+    if flashcard:
+        if flashcard.user_id == current_user.id:
+            db.session.delete(flashcard)
+            db.session.commit()
+
+        fetched_flash.title = None           #when we delete a note we don't want to reload the current flash card with previous
+    return jsonify({})
+
+@views.route("/fetch-deck", methods = ["POST", "GET"])
+def fetch_deck():
+    print('fetch_deck method ' + request.method)
+    showF = json.loads(request.data)
+    # showFl = showF["showFlash"]
+    print(showF)
+    return jsonify({})
+
+
+@views.route("/fetch-flash", methods = ["POST", "GET"])
+def fetch_flash():
+    print('fetch_flash method ' + request.method)
+    flashcard = json.loads(request.data)
+    flashId = flashcard["flashId"]
+    flashcard = Flashcard.query.get(flashId)
+    #deck = Deck.query.get(note.user_deck_id)
+    # note = json.loads(request.data)
+    # noteId = note["noteId"]
+    # note = Note.query.get(noteId)
+    
+    
+    # note.title = "itsss"
+    # db.session.commit()
+
+    # print(note.title)
+    # print(request.data)
+    
+    # return render_template("home.html", user = current_user, test = "testfetch") 
+    
+    # print(jsonify(note.data))
+    #return render_template("home.html", user = current_user, test = note.data)  
+    fetched_flash.title = flashcard.title
+    fetched_flash.data = flashcard.data
+    fetched_flash.data2 = flashcard.data2
+    fetched_flash.deck_title = flashcard.deck_title
+    
+    return jsonify({})
+
+
+
+
+
+
+
+
+
+# my old home page 
+@views.route('/home_old', methods = ["GET", "POST"])       #this is homepage route(url)
+@login_required
+def home_old():
+    print('home method ' + request.method)
     if request.method == "POST":
         deck_title = request.form.get("deck_title")
         flash_title = request.form.get("flash_title")
         flasha = request.form.get("f_front")
         flashb = request.form.get("f_back")
-
+        print(request.form)
         
         if len(deck_title) < 1:
             flash("Deck must have a title!", category = "error")
@@ -54,93 +251,4 @@ def home():
         f_data2 = fetched_flash.data2
         f_deck_title = fetched_flash.deck_title
 
-    return render_template("home.html", user = current_user, flash_title = f_title, f_front = f_data, f_back = f_data2, deck_title = f_deck_title)
-
-#
-# deletes deck and all flashcards.  Will want to change this as to allow a flash card toe xist in multiple decks. 
-#
-@views.route("delete-deck", methods = ["POST", "GET"])
-def delete_deck():
-    deck = json.loads(request.data)
-    deckId = deck["deckId"]
-
-    deck = Deck.query.get(deckId)
-    if deck:
-        if deck.user_id == current_user.id:
-            for flashcard in deck.flashcards:
-                db.session.delete(flashcard)
-            db.session.delete(deck)
-            db.session.commit()
-    
-    return jsonify({})
-
-#
-# deletes a single flash card. 
-#
-@views.route("/delete-flash", methods = ["POST", "GET"])
-def delete_note():
-    flashcard = json.loads(request.data)
-    flashId = flashcard["flashId"]
-    flashcard = Flashcard.query.get(flashId)
-    #print(note.user.email)                     demonstrates how backref works(see models.py and db.relationship(...))
-    if flashcard:
-        if flashcard.deck.user_id == current_user.id:
-            db.session.delete(flashcard)
-            db.session.commit()
-
-        fetched_flash.title = None           #when we delete a note we don't want to reload the current flash card with previous
-    return jsonify({})
-
-@views.route("/fetch-deck", methods = ["POST", "GET"])
-def fetch_deck():
-    showF = json.loads(request.data)
-    # showFl = showF["showFlash"]
-    print(showF)
-    return jsonify({})
-
-
-@views.route("/fetch-flash", methods = ["POST", "GET"])
-def fetch_flash():
-    print('hit')
-    flashcard = json.loads(request.data)
-    flashId = flashcard["flashId"]
-    flashcard = Flashcard.query.get(flashId)
-    #deck = Deck.query.get(note.user_deck_id)
-    # note = json.loads(request.data)
-    # noteId = note["noteId"]
-    # note = Note.query.get(noteId)
-    
-    
-    # note.title = "itsss"
-    # db.session.commit()
-
-    # print(note.title)
-    # print(request.data)
-    
-    # return render_template("home.html", user = current_user, test = "testfetch") 
-    
-    # print(jsonify(note.data))
-    #return render_template("home.html", user = current_user, test = note.data)  
-    fetched_flash.title = flashcard.title
-    fetched_flash.data = flashcard.data
-    fetched_flash.data2 = flashcard.data2
-    fetched_flash.deck_title = flashcard.deck_title
-    
-    return jsonify({})
-
-@views.route('/test', methods=['GET', 'POST'])
-def testfn():
-    return render_template("test.html", user = current_user, testvar = "it worked")
-#     # GET request
-#     if request.method == 'GET':
-#         message = {'greeting':'Hello from Flask!'}
-#         return jsonify(message)  # serialize and use JSON headers
-#     # POST request
-#     if request.method == 'POST':
-#         print(request.get_json())  # parse as JSON
-#         return 'Sucesss', 200
-#     return redirect("/")    
-
-@views.route('/managedecks', methods=['GET', 'POST'])
-def managedecks():
-    return render_template("managedecks.html", user = current_user)
+    return render_template("home_old.html", user = current_user, flash_title = f_title, f_front = f_data, f_back = f_data2, deck_title = f_deck_title)
